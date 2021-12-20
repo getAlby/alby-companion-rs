cff6955d0b60097638ac34c64a8827666fdad692
oz <oz@jamm.dev>
2021-12-20 14:33:12 +0100

    Fix chrome_native_messaging handling

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-use std::io;
-
 use chrome_native_messaging::event_loop;
 use serde::Serialize;
-use serde_json::json;
+use serde_json::Value as SerdeValue;
 
 #[derive(Serialize)]
 struct BasicMessage<'a> {
     payload: &'a str
 }
 
+fn handler<'a>(_: SerdeValue) -> Result<BasicMessage<'a>, String> {
+    Ok(BasicMessage { payload: "Hello, World!" })
+}
+
 fn main() {
-    event_loop(|value| match value {
-        Null => Err("null payload"),
-        _ => Ok(BasicMessage { payload: "Hello, World!" })
-    });
+    event_loop(handler);
 }
