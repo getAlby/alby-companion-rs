09d2242d3f3d8351801d646abbc0cf2a80ef5e66
oz <oz@jamm.dev>
2021-12-21 13:32:36 +0100

    Random ports for Tor: 19050 to 29050 close #2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate chrono;
 
+use std::borrow::Borrow;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::time::SystemTime;
@@ -8,6 +9,7 @@ use chrome_native_messaging::event_loop;
 use chrono::DateTime;
 use chrono::offset::Utc;
 use libtor::{Tor, TorFlag};
+use rand::{Rng, thread_rng};
 use serde::Serialize;
 use serde_json::Value as SerdeValue;
 
@@ -26,13 +28,16 @@ fn handler(v: SerdeValue) -> Result<ResMessage, String> {
     Ok(ResMessage { payload: response })
 }
 
+thread_local!(static TOR_PORT: u16 = get_random_port());
+
 fn main() {
     println!("🚧 Debug log: /tmp/alby-rs.log");
-    write_debug(format!("Starting Tor"));
+    write_debug(format!("Starting Tor on {}", get_tor_port()));
+
     Tor::new()
         .flag(TorFlag::DataDirectory("/tmp/tor-rust".into()))
         .flag(TorFlag::ControlPort(0))
-        .flag(TorFlag::SocksPort(19050))
+        .flag(TorFlag::SocksPort(get_tor_port()))
         .start_background();
 
     write_debug(format!("Waiting for messages"));
@@ -40,7 +45,7 @@ fn main() {
 }
 
 fn get_response(message: SerdeValue) -> Result<String, reqwest::Error> {
-    let proxy = reqwest::Proxy::all("socks5h://127.0.0.1:19050")?;
+    let proxy = reqwest::Proxy::all(&format!("socks5h://127.0.0.1:{}", get_tor_port()))?;
     let client = reqwest::blocking::Client::builder().proxy(proxy).build()?;
 
     // This code is needed just to "use" a `value` variable
@@ -73,4 +78,13 @@ fn write_debug(msg: String) {
     if let Err(e) = writeln!(file, "{}\t {}", utc.format("%d-%m-%Y %H:%M:%S"), msg) {
         eprintln!("Couldn't write to file: {}", e);
     }
+}
+
+fn get_random_port() -> u16 {
+    let mut rng = thread_rng();
+    rng.gen_range(19050..29051)
+}
+
+fn get_tor_port() -> u16 {
+    TOR_PORT.with(|tor_port| tor_port.borrow().clone())
 }
